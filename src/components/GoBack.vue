e6d7158d481f1a58fa8856e72cbe1e76eadb647d
Sasha <646415@student.inholland.nl>
2023-09-28 20:25:52 +0200

    add goback button

---
+++ src/components/GoBack.vue
@@ -0,0 +1,16 @@
+<template>
+    <span class="go-back">
+        <button @click="router.back()">Go Back</button>
+    </span>
+</template>
+
+<!-- ignore useless error -->
+<script>
+export default {
+    methods: {
+        goBack() {
+            this.$router.back();
+        },
+    },
+};
+</script>
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,6 +1,7 @@
 <template>
     <section v-if="destination" class="destination">
         <h1>{{ destination.name }}</h1>
+        <GoBack />
         <div class="destination-details">
             <img :src="`/images/${destination.image}`" :alt="destination.name">
             <p>{{ destination.description }}</p>
@@ -20,9 +21,11 @@
 
 <script>
 import ExperienceCards from '../components/ExperienceCards.vue'
+import GoBack from '../components/GoBack.vue'
+
 export default {
     components: {
-        ExperienceCards
+        ExperienceCards, GoBack
     },
     props: {
         id: { type: Number, required: true }
