38b69632634c06d8e6f82ad5ba9a96d384ca4111
Sasha <646415@student.inholland.nl>
2023-09-29 17:19:13 +0200

    route guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import sourceData from '../data.json'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,19 @@ const router = createRouter({
       name: 'destination.show',
       component: () => import('../views/DestinationShow.vue'),
       props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+      beforeEnter(to) {
+        const exists = sourceData.destinations.find(
+          (destination) => destination.id === parseInt(to.params.id)
+        )
+        if (!exists)
+          return {
+            name: 'NotFound',
+            //allow keeping the url
+            params: { pathMatch: to.path.split('/').slice(1) },
+            query: to.query,
+            hash: to.hash
+          }
+      },
       children: [
         {
           path: ':experienceSlug',
