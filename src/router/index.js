3103955510c10164e64d42aedfa560c8fcdf6b86
Sasha <646415@student.inholland.nl>
2023-10-02 16:39:44 +0200

    redirect and alias

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,8 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      alias: '/home'
     },
     {
       path: '/protected',
